0677c23bf3f37f952aa9e459d2767048b879ccf3
chloe-ui218 <chloerhinan10@gmail.com>
2024-09-14 11:18:38 -1200

    feat: understanding properties and methods

--- week 12/version1.js
+++ week 12/version1.js
@@ -12,7 +12,7 @@ class Task {
 
     displayDetails() {
         console.log("+-------------------------------------------------------------------------+");
-        this.category(`\tCategory: ${this.category}`);
+        console.log(`\tCategory: ${this.category}`);
         console.log(`\tTask: ${this.title}`);
         console.log(`\tDescription: ${this.description}`);
         // console.log(`\tCompleted: ${this.completed}`);
@@ -24,10 +24,26 @@ class Task {
         console.log(`${this.completed ? "Task is Completed" : "\tTask is not completed"}`);
         console.log("+--------------------------------------------------------------------------+");
     }
+
+    // updateTask(newTitle, newDescription, newCategory) {
+    //    this.title = newTitle;
+    //    this.description = newDescription;
+    //    this.category = newCategory;
+    //    console.log("Task updated successfully");
+    // }
+
+    updateTaskCategory(newCategory) {
+        this.title = newCategory;
+        console.log("Task Category updated successfully");
+    }
 }
 
-let task1 = new Task("Buy groceries", "Get milk, eggs, and bread from the store", "Shopping");
+let task1 = new Task("Buy groceries", "Get milk, eggs, and bread from the store.", "Shopping");
 
+task1.displayDetails();
+
+task1.updateTaskTitle("Buy groceries - 1");
+console.log("\t Updated Task Details \t");
 task1.displayDetails();
 
 task1.toggleCompleted();
