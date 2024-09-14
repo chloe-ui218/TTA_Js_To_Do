02c8605d93be1aa16971d5d4d7fed14766ed7230
chloe-ui218 <chloerhinan10@gmail.com>
2024-09-13 19:37:34 -1200

    chore: understood properties of oop

--- week 12/version1.js
+++ week 12/version1.js
@@ -1,29 +1,32 @@
 class Task {
-    constructor(title, description) {
-        this.little = title;
+    constructor(title, description, category) {
+        this.title = title;
         this.description = description;
+        this.category = category;
         this.completed= false;
     }   
     
-    toggle() {
+    toggleCompleted() {
         this.completed = !this.completed;
     }
 
     displayDetails() {
-        console.log("+------------------------------------------------------------+");
+        console.log("+-------------------------------------------------------------------------+");
+        this.category(`\tCategory: ${this.category}`);
         console.log(`\tTask: ${this.title}`);
         console.log(`\tDescription: ${this.description}`);
         // console.log(`\tCompleted: ${this.completed}`);
-        if (this.completed) {
-            console.log("\tTask is Completed"); 
-        } else{
-            console.log("\tTask is Not Completed");
-        }
-        console.log("+------------------------------------------------------------+");
+        // if (this.completed) {
+        //    console.log("\tTask is Completed"); 
+        // } else{
+        //     console.log("\tTask is Not Completed");
+        // }
+        console.log(`${this.completed ? "Task is Completed" : "\tTask is not completed"}`);
+        console.log("+--------------------------------------------------------------------------+");
     }
 }
 
-let task1 = new Task("Buy groceries", "Get milk, eggs, and bread from the store.");
+let task1 = new Task("Buy groceries", "Get milk, eggs, and bread from the store", "Shopping");
 
 task1.displayDetails();
 
