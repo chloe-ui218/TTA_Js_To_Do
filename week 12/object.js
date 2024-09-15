9844ce6552c4571bf6067a3fa91fd522aa9802b4
chloe-ui218 <chloerhinan10@gmail.com>
2024-09-14 13:54:54 -1200

    feat: introducing and understanding factory functions and their properties

--- week 12/object.js
+++ week 12/object.js
@@ -17,8 +17,40 @@ function createTask(title, description, category){
             console.log(`\tDescription: ${this.description}`);
             console.log(`${this.completed ? "Task is Completed" : "\tTask is not completed"}`);
             console.log("+-----------------------------------------------------------------------+");
+        },
+
+        updateTitle(newTitle) {
+            this.title = newTitle;
+            console.log("Task Title updated successfully");
+
+        },
+
+        updateCategory(newCategory) {
+            this.category = newCategory;
+            console.log("Task Category updated successfully")
         }
     }
 }
 
 let task1 = createTask("Buy groceries", "Get milk, eggs, and bread from the store.", "Shopping");
+task1.displayDetails();
+
+task1.toggleCompleted();
+console.log("| \t New Task Details \t |");
+task1.displayDetails();
+
+task1.updateTitle("Buy groceries - 1");
+console.log("\t Updated Task Details \t");
+task1.displayDetails();
+
+task1.updateDescription("1. Get milk, eggs, and bread from the store.");
+console.log("\t Updated Task Details \t");
+task1.displayDetails();
+
+task1.updateCategory("shopping - 1");
+console.log("\t Updated Task Details \t");
+task1.displayDetails();
+
+// task1.toggleCompleted();
+// console.log("| \t New Task Details \t |");
+// task1.displayDetails();
